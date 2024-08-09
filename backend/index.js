c455996d7468b5eddc00570f4443a96a8c99f5bb
kratika125 <kratikabhadauria21@gmail.com>
2024-08-09 15:07:59 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";   
+const app=express();
+const PORT=3000;
+//middleware
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions={
+    origin:'http//localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+app.listen(PORT,()=>{
+    console.log("Server running at port ${PORT} ");
+})
